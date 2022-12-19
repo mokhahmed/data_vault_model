3c1a4272ebc6202fce9f1d5c21021b219fea5ccf
Moukhtar Ahmed <mo@moka.joonix.net>
2022-12-19 23:00:11 +0000

    adding more models

--- includes/dv_models.js
+++ includes/dv_models.js
@@ -54,8 +54,113 @@ var users_model =  [
 var users_model_doc  = {}
 users_model.forEach(el => { users_model_doc[el.target_column_name+""] = el.description});
 
+var bikes_model = [
+  {
+    "source_column_name": "bike_id",
+    "source_data_type": "integer",
+    "target_column_name": "bike_id",
+    "target_data_type": "integer",
+    "is_key": "Y", 
+    "is_calculated": "N",
+    "calculated_by": "",
+    "description": "Bike ID"
+  },
+  {
+    "source_column_name": "bike_type",
+    "source_data_type": "string",
+    "target_column_name": "bike_type",
+    "target_data_type": "string",
+    "is_key": "N", 
+    "is_calculated": "N",
+    "calculated_by": "",
+    "description": "Bike Type"
+  }
+]
+
+var bikes_model_doc  = {}
+bikes_model.forEach(el => { bikes_model_doc[el.target_column_name+""] = el.description});
+
+var trips_model = [
+  {
+    "source_column_name": "bike_id",
+    "source_data_type": "integer",
+    "target_column_name": "bike_id",
+    "target_data_type": "integer",
+    "is_key": "Y", 
+    "is_calculated": "N",
+    "calculated_by": "",
+    "description": "Bike ID"
+  },
+  {
+    "source_column_name": "user_id",
+    "source_data_type": "integer",
+    "target_column_name": "user_id",
+    "target_data_type": "integer",
+    "is_key": "Y", 
+    "is_calculated": "N",
+    "calculated_by": "",
+    "description": "User ID"
+  },
+   {
+    "source_column_name": "start_station_id",
+    "source_data_type": "integer",
+    "target_column_name": "start_station_id",
+    "target_data_type": "integer",
+    "is_key": "Y", 
+    "is_calculated": "N",
+    "calculated_by": "",
+    "description": "Start Station ID"
+  },
+  {
+    "source_column_name": "end_station_id",
+    "source_data_type": "integer",
+    "target_column_name": "end_station_id",
+    "target_data_type": "integer",
+    "is_key": "Y", 
+    "is_calculated": "N",
+    "calculated_by": "",
+    "description": "End Station ID"
+  },
+   {
+    "source_column_name": "start_time",
+    "source_data_type": "datetime",
+    "target_column_name": "start_time",
+    "target_data_type": "datetime",
+    "is_key": "N", 
+    "is_calculated": "N",
+    "calculated_by": "",
+    "description": "Start Time, in NYC local time."
+  },
+  {
+    "source_column_name": "stop_time",
+    "source_data_type": "datetime",
+    "target_column_name": "stop_time",
+    "target_data_type": "datetime",
+    "is_key": "N", 
+    "is_calculated": "N",
+    "calculated_by": "",
+    "description": "Stop Time, in NYC local time."
+  },
+   {
+    "source_column_name": "trip_duration",
+    "source_data_type": "integer",
+    "target_column_name": "trip_duration",
+    "target_data_type": "integer",
+    "is_key": "N", 
+    "is_calculated": "N",
+    "calculated_by": "",
+    "description": "Trip Duration (in seconds)"
+  }
+]
+
+var trips_model_doc  = {}
+trips_model.forEach(el => { trips_model_doc[el.target_column_name+""] = el.description});
 
 module.exports = {
   users_model,
-  users_model_doc
+  users_model_doc,
+  bikes_model,
+  bikes_model_doc,
+  trips_model,
+  trips_model_doc
 }
